5be950dcafd6a439e678fb5508fac0bc60b9584c
Alejandro Daniel Espinosa Chim <119325061+DanielEspinosaChim@users.noreply.github.com>
2024-03-25 16:16:01 -0600

    Update 2-Downsampling.py

--- 2-Downsampling.py
+++ 2-Downsampling.py
@@ -1,16 +1,13 @@
 import csv
 
-# Nombre del archivo de entrada y salida
 archivo_entrada = 'frequencies_malignos.csv'
 archivo_salida = 'DownMalignos.csv'
 
-# Abrir el archivo de entrada y el archivo de salida
 with open(archivo_entrada, mode='r') as infile, open(archivo_salida, mode='w', newline='') as outfile:
     reader = csv.reader(infile)
     writer = csv.writer(outfile)
     
     for row in reader:
-        # Asumiendo que la frecuencia es el segundo elemento de cada fila
         frecuencia = int(row[1])
         # Ajustar la frecuencia si es mayor a 100
         if frecuencia > 38:
